80d73aa30dc53e5117c0c95d3d77e677b8869bb7
Matt Spurlin <mspurlin@ics-llc.net>
2017-05-16 21:58:25 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # geeklybot
 A chat bot meant to be used with the GeeklyInc Twitch chat!
+
+Required environment variables (these will eventually just be handled by Docker):
+USERNAME - Twitch username
+PASS_PATH - Path to a file contain a single line of text that is your password from https://twitchapps.com/tmi/
+CHANNEL - the twitch channel to join
---
+++ bot/src/server.js
@@ -0,0 +1,23 @@
+var irc = require('irc');
+fs = require('fs');
+
+var pass = fs.readFileSync(process.env.PASS_PATH).toString().trim();
+
+var channel = process.env.CHANNEL;
+
+var bot = new irc.Client('irc.chat.twitch.tv',process.env.USERNAME, {
+    channels: ['#' + channel],
+    port: 6667,
+    password: pass,
+    debug: true,
+    showErrors: true,
+    autoRejoin: true, 
+    autoConnect: true,
+});
+bot.addListener('error', function(message) {
+    console.log('error: ', message);
+});
+
+bot.addListener('message#' + channel, function (from, message) {
+    console.log(from + ' => #' + channel + ': ' + message);
+}
