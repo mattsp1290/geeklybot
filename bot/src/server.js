9402193f47319e74244b5d19669dda790494d1d6
Matt Spurlin <mspurlin@ics-llc.net>
2017-05-16 22:50:53 -0500

    Add support for bits!

--- bot/src/server.js
+++ bot/src/server.js
@@ -5,6 +5,8 @@ var pass = fs.readFileSync(process.env.PASS_PATH).toString().trim();
 
 var channel = process.env.CHANNEL;
 
+var bitTypes = ['cheer', 'swiftrage', 'kappa', 'kreygasm'];
+
 var bot = new irc.Client('irc.chat.twitch.tv',process.env.USERNAME, {
     channels: ['#' + channel],
     port: 6667,
@@ -24,5 +26,18 @@ bot.addListener('message#' + channel, function (from, message) {
     // Maybe break apart the message if it is !check stuff
     // Fire the check event with data {message: message, from: from}
     // Probably trim off !check from the message first
+    bitTypes.forEach(function(bitType) {
+      if (message.indexOf(bitType) != -1) {
+        // Example here we should emit a bit event.
+        console.log('Message: You got a bit of type ' + bitType);
+      }
+    });
     console.log(from + ' => #' + channel + ': ' + message);
+});
+
+bot.addListener('raw', function (data) {
+    // These are using for testing against raw.
+    // console.log('raw: ' + data.command + " : " + data.args);
+    // console.log('prefix: ' + data.prefix + ' rawCommand: ' + data.rawCommand + ' commandType: ' + data.commandType);
+    // console.log(Object.keys(data));
 });
